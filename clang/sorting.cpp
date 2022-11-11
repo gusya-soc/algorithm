07b687b50b4b5b8f55a55705b9db15851f1df8dd
gusya-soc <starfallingfromsky@gmail.com>
2022-11-11 06:00:14 +0000

    updata sorting

--- clang/sorting.cpp
+++ clang/sorting.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 
-#define R 100 //random range
-#define arraySize 10 // array size
+#define R 100//random range
+#define arraySize 100 // array size
 
 
 
@@ -14,6 +14,10 @@ int* insert_swap();
 void swap(int* a,int* b);
 void show();
 void checker();
+void merge_merge(int a[],int s,int m,int e);
+void heap_up(int a[],int idx);
+void heap_down(int a[],int idx);
+
 
 void swap(int*a ,int* b)
 {
@@ -109,6 +113,130 @@ int* insert_swap(int a[],int size)
     return a;
 }
 
+void quick(int a[],int s,int e)
+{
+    if(s>=e)
+    {
+        return;
+    }
+    
+    int pos;
+    int piv = a[s];
+    int i = s;int j = e;
+
+
+    // get the true position
+    while(i<j)
+    {
+        while(i<j && a[j]>=piv)
+        {
+            j--;
+        }
+        a[i] = a[j];
+        while(i<j && a[i]<=piv)
+        {
+            i++;
+        }
+        a[j]=a[i];
+    }
+    a[i]=piv;
+    pos = i;
+
+
+    quick(a,s,pos-1);
+    quick(a,pos+1,e);
+}
+
+void merge_divide(int a[],int s,int e)
+{
+    if(s>=e)
+    {
+        return;
+    }
+    int mid = (s+e)/2;
+    merge_divide(a,s,mid);
+    merge_divide(a,mid+1,e);
+    merge_merge(a,s,mid,e);
+
+}
+void merge_merge(int a[],int s,int m,int e)
+{
+    int i = s;
+    int j = m+1;
+    int index = 0;
+    int tmp[e+1];
+
+    //有序数组合并
+    while(i<=m && j<=e)
+    {
+        if(a[i]<a[j])
+        {
+            tmp[index++] = a[i++];
+        }
+        else
+        {
+            tmp[index++] = a[j++];
+        }
+    }
+    while(i<=m)
+    {
+        tmp[index++] = a[i++];
+    }
+    while(j<=e)
+    {
+        tmp[index++] = a[j++];
+    }
+
+
+    // 复制回原数组
+    index = 0;
+    while(s<=e)
+    {
+        a[s++] = tmp[index++];
+    }
+}
+
+void heap(int a[],int size)
+{
+    for(int i = 1;i<size;i++)
+    {
+        heap_up(a,i);
+    }
+    for(int i=size-1;i>=1;i--)
+    {
+        heap_down(a,i);
+    }
+}
+void heap_up(int a[],int idx)
+{
+    int tmp = a[idx];
+    int i = idx;
+    for(i;i>=1&&tmp>a[(i-1)/2];i = (i-1)/2)
+    {
+        a[i] = a[(i-1)/2];
+    }
+    a[i] = tmp;
+}
+void heap_down(int a[],int idx)
+{
+    swap(&a[0],&a[idx]);
+    // int tmp = a[0];
+    int node = 0;
+    int child = 1;
+    while(child<idx)
+    {
+        if(child+1<idx && a[child+1]>a[child])
+        {
+            child = child+1;
+        }
+        if(a[node]<a[child])
+        {
+            swap(&a[node],&a[child]);
+        }
+        node = child;
+        child = 2*child+1;
+    }
+}
 
 int* creatRandomArray(int n)
 {
@@ -123,7 +251,7 @@ void show(int a[])
 {
     for(int i=0;i<arraySize;i++)
     {
-        printf("%d,",a[i]);
+        printf("%d ",a[i]);
     }
     printf("\n");
 }
@@ -140,11 +268,35 @@ int main()
     // show(b);
     // checker(b,arraySize);
 
-    int* c = creatRandomArray(arraySize);
-    insert_swap(c,arraySize);
-    show(c);
-    int* d = creatRandomArray(arraySize);
-    insert_shift(d,arraySize);
-    show(d);
+    // int* c = creatRandomArray(arraySize);
+    // insert_swap(c,arraySize);
+    // show(c);
+    // int* d = creatRandomArray(arraySize);
+    // insert_shift(d,arraySize);
+    // show(d);
+    // quick(d,0,arraySize-1);
+    // show(d);
+    // checker(d,arraySize);
+
+
+    // int* e = creatRandomArray(arraySize);
+    // show(e);
+    // merge_divide(e,0,arraySize-1);
+    // show(e);
+    // checker(e,arraySize);
+
+    // int t[8] = {0,3,6,4,2,5,1,7};
+    // heap_up(t,7);
+    // show(t);
+    // for(int i=0;i<8;i++)
+    // {
+    //     heap_up(t,i);
+    // }
+    // show(t);
+    int* f = creatRandomArray(arraySize);
+    show(f);
+    heap(f,arraySize);
+    show(f);
+    checker(f,arraySize);
     return 0;
 }
---
+++ clang/test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int i = 10;
+    while(i--)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    i = 10;
+    while(--i)
+    {
+        printf("%d ",i);
+    }
+}
